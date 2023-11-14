12853ec5af705360ec2db8849a240eb49d1e4e32
Ginger Owen <gingerowen0421@gmail.com>
2023-11-13 17:20:09 -0700

    logic to get rand chc and determine points

--- index.js
+++ index.js
@@ -3,4 +3,24 @@ const loses = 0;
 
 const choices = ["Rock", "Paper", "Scissors"];
 
+const randomChoice =  choices[Math.floor(Math.random()*choices.length)]
+
+console.log(randomChoice);
+
+function game(params) {
+    if (player === randomChoice) {
+        return "draw"
+    } 
+    if (player === "Rock" && randomChoice === "Scissors" || //for player wins
+        player === "Scissors" && randomChoice === "Paper" ||
+        player === "Paper" && randomChoice === "Rock") {
+            wins =+ 1
+        return "Win!"
+    } else {
+        loses =-1
+        return "You lose:-("
+
+    }
+}
+
 
