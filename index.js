b77e7d477d3f5a4b0872ee0abe146e9c3103d0db
Ginger Owen <gingerowen0421@gmail.com>
2023-12-31 10:40:54 -0700

    added eventlistener when window loads

--- index.js
+++ index.js
@@ -7,28 +7,31 @@ const randomChoice =  choices[Math.floor(Math.random()*choices.length)]
 
 console.log(randomChoice);
 
-function game(params) {
-    if (player === randomChoice) {
-        return "draw"
-    } 
-    if (player === "Rock" && randomChoice === "Scissors" || //for player wins
-        player === "Scissors" && randomChoice === "Paper" ||
-        player === "Paper" && randomChoice === "Rock") {
-            wins =+ 1
-        return "Win!"
-    } else {
-        loses =-1
-        return "You lose:-("
+window.addEventListener("load", confirm("Would you like to play?"))
 
-    }
-}
-//end game()
-//prompt is for choice r p s
 
-//alert is for score
-alert(loses, wins)
-//confirm is for do you wnat to play has ok (true) or cancel
-confirm("Would you like to play rock, paper, scissors?") // need to add theis to an onload
+// function game(params) {
+//     if (player === randomChoice) {
+//         return "draw"
+//     } 
+//     if (player === "Rock" && randomChoice === "Scissors" || //for player wins
+//         player === "Scissors" && randomChoice === "Paper" ||
+//         player === "Paper" && randomChoice === "Rock") {
+//             wins =+ 1
+//         return "Win!"
+//     } else {
+//         loses =-1
+//         return "You lose:-("
 
-window.confirm()//possible switch statement for options confirm need to be in if statement???
+//     }
+// }
+// //end game()
+// //prompt is for choice r p s
+
+// //alert is for score
+// alert(loses, wins)
+// //confirm is for do you wnat to play has ok (true) or cancel
+// confirm("Would you like to play rock, paper, scissors?") // need to add theis to an onload
+
+// window.confirm()//possible switch statement for options confirm need to be in if statement???
 
