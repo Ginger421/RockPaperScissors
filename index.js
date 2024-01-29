9472e7ff483fbd90e627f4f86bfaa968ec33b128
Ginger Owen <gingerowen0421@gmail.com>
2024-01-29 13:42:06 -0700

    changed var names and cleaned up code

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
 const choices = ["r", "p", "s"];
 
-const player = ["r", "p", "s"]
-
 const randomChoice =  choices[Math.floor(Math.random()*choices.length)]
 
 window.onload = (Event) => {
-    let text;
     if (confirm("Would you like to play?") == true) {
-        let game = prompt("Please enter r p or s" , "");
-        game;
+        let player = prompt("Please enter r p or s" , "");
+        console.log(player);
+        promptCode();
     }   else {
-        return text = "You chose not to play!"
+        document.getElementById("text").innerHTML = "You chose not to play!"
     } //end ifelse
-
-    console.log(game);
 
 } //end onload
 
 
 
-// function promptCode () {
-//     let wins = 0;
-//     let loses = 0;
-//     prompt("please enter r p s"); //am i grabbing this?
-//     console.log(prompt)
-//     if (player === randomChoice) {
-//                 return "draw"
-//             } 
-//             if (player === "r" && randomChoice === "s" || //for player wins
-//                 player === "s" && randomChoice === "p" ||
-//                 player === "p" && randomChoice === "r") {
-//                     wins = wins=+1;
-//                     console.log(wins);
-//                 return "Win!"
-//             } else {
-//                 loses = loses =+ 1;
-//                 return "You lose:-("
-//                 console.log(loses);
+function promptCode () {
+    let wins = 0;
+    let loses = 0;
+    if (player === randomChoice) {
+                return "draw"
+            } 
+            if (player === "r" && randomChoice === "s" || //for player wins
+                player === "s" && randomChoice === "p" ||
+                player === "p" && randomChoice === "r") {
+                    wins = wins=+1;
+                    console.log(wins);
+                return "Win!"
+            } else {
+                loses = loses =+ 1;
+                return "You lose:-("
+                console.log(loses);
         
-//             }
+            }
 
-// }
+}
 
 // //alert is for score
 // alert(loses, wins)
