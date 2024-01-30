01f8712b8ca181b45e032b2d78ee17b4fb5d3ede
Ginger Owen <gingerowen0421@gmail.com>
2024-01-30 14:05:17 -0700

    only need logic to play another round of gave

--- index.js
+++ index.js
@@ -1,50 +1,56 @@
 const choices = ["r", "p", "s"];
 
-let player
+//var player;
 
 const randomChoice =  choices[Math.floor(Math.random()*choices.length)]
+start();
 
-window.onload = (Event) => {
+function start() {
     if (confirm("Would you like to play?") == true) {
-        player = prompt("Please enter r p or s" , "");
+        let player = prompt("Please enter r p or s");
+        game(player);
+        function game(player) {
         console.log(player);
-        game();
+            let wins = 0;
+            let loses = 0;
+            switch (true) {
+            
+                case player === randomChoice:
+                    alert("Draw!");
+                    confirm("play again?")
+                    // if (confirm) {
+                    //     player;
+                    // } else {
+                    //     return;
+                    // }
+                    break;
+            
+                case player === "r" && randomChoice === "s":
+                case player === "ss" && randomChoice === "p":
+                case player === "p" && randomChoice === "r":
+                    wins = wins + 1 
+                    alert(`You win! 
+                            wins:${wins}`);
+                    confirm("play again")
+                    break;
+            
+                case player === "s" && randomChoice === "r":
+                case player === "p" && randomChoice === "s":
+                case player === "r" && randomChoice === "p":
+                    loses = loses + 1;
+                    alert(`You loose!
+                            losses:${loses}`)
+                    confirm("play again?");
+                    break;
+            
+                default:
+                    alert("That is not a valid selection. Please enter r p or s if you would like to play");
+                    break;
+            } //end switch
+        } // end game()
     }   else {
         document.getElementById("text").innerHTML = "You chose not to play!"
     } //end ifelse
+} //end start()
 
-} //end onload
-
-
-function game() {  
-
-let wins = 0;
-let loses = 0;
-switch (player) {
-
-    case player === randomChoice:
-        alert("Draw!");
-        break;
-
-    case player === "r" && randomChoice === "s":
-    case player === "s" && randomChoice === "p":
-    case player === "p" && randomChoice === "r":
-        wins = wins + 1 
-        alert(`You win! 
-                wins:${wins}`)
-        break;
-
-    case player === "s" && randomChoice === "r":
-    case player === "p" && randomChoice === "s":
-    case player === "r" && randomChoice === "p":
-        loses = loses + 1;
-        alert(`You loose!
-                losses:${loses}`)
-        break;
-
-    default:
-        alert("That is not a valid selection. Please enter r p or s if you would like to play");
-        break;
-}
-} //end game()
 
